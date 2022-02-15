e74622904e46c81629b948cfd09b23aac896becf
Tri <vutriminh@gmail.com>
2022-02-15 09:34:41 -0500

    initialize hw3 and the work

---
+++ hw3.py
@@ -0,0 +1,65 @@
+# Tri Vu HW #3
+
+#? Functions
+
+#? 1. Find the smallest number
+# Write a function smallest that accepts a List of numbers as an argument.
+# It should return the smallest number in the List.
+
+# nums = [23, 2, 17, 3, 13, 5, 11, 7]
+
+# def smallest(numsList):
+#     min = numsList[0]
+#     for x in numsList:
+#         if x < min:
+#             min = x
+#     return min
+
+# print(smallest(nums))
+
+
+#? 2. Find the largest number
+# Write a function largest that accepts a List of numbers as an argument.
+# It should return the largest number in the List.
+
+# nums = [2, 17, 3, 13, 5, 11, 7]
+
+# def largest(numsList):
+#     max = numsList[0]
+#     for x in numsList:
+#         if x > max:
+#             max = x
+#     return max
+
+# print(largest(nums))
+
+
+#? 3. Find the shortest string
+# Write a function shortest that accepts a List of Strings as an argument.
+# It should return the shortest String in the List.
+
+# fruits = ["apple", "orange", "pear", "watermelon", "coconut"]
+
+# def shortest(fruitsList):
+#     shortestFruit = fruitsList[0]
+#     for x in fruitsList:
+#         if len(x) < len(shortestFruit):
+#             shortestFruit = x
+#     return shortestFruit
+
+# print(shortest(fruits))
+
+#? 4. Find the longest string
+# Write a function longest that accepts a List of Strings as an argument.
+# It should return the longest String in the List.
+
+# fruits = ["apple", "orange", "pear", "watermelon", "coconut"]
+
+# def shortest(fruitsList):
+#     shortestFruit = fruitsList[0]
+#     for x in fruitsList:
+#         if len(x) > len(shortestFruit):
+#             shortestFruit = x
+#     return shortestFruit
+
+# print(shortest(fruits))
